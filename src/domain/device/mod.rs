97f320e836b8e047e66116014853ccff89d5fd3d
Migo <migo@Migos-MacBook-Pro.local>
2024-02-02 18:15:13 +0900

    add temperature saving logic

--- src/domain/device/mod.rs
+++ src/domain/device/mod.rs
@@ -28,11 +28,34 @@ impl DeviceAggregate {
         }
     }
 
-    pub fn save_temperatures(&mut self, cmd: SaveDeviceTemperature) {
-        todo!()
+    // ? How are you going to make sure of idempotency
+    pub fn save_temperatures(&mut self, cmd: SaveDeviceTemperature) -> Result<(), Error> {
+        // To prevent frequent allocation
+        self.temperatures =
+            Vec::with_capacity(f64::ceil(cmd.temperatures.len() as f64 / 4.0) as usize);
+
+        let mut from = 0;
+        while from < cmd.temperatures.len() {
+            let chunk: String = cmd
+                .temperatures
+                .chars()
+                .into_iter()
+                .skip(from)
+                .take(4)
+                .collect();
+
+            self.temperatures.push(DeviceTemperature::new(
+                self.device_id,
+                cmd.registered_at,
+                &chunk.to_string(),
+            )?);
+            from += 4;
+        }
+        Ok(())
     }
 }
 
+#[derive(Clone)]
 pub struct DeviceTemperature {
     pub device_id: i64,
     pub temperature: i16,
@@ -40,6 +63,18 @@ pub struct DeviceTemperature {
 }
 
 impl DeviceTemperature {
+    pub fn new(
+        device_id: i64,
+        checked_at: DateTime<Utc>,
+        tempature_in_hex: &str,
+    ) -> Result<Self, Error> {
+        Ok(Self {
+            device_id,
+            checked_at,
+            temperature: Self::hex_to_decimal(tempature_in_hex)?,
+        })
+    }
+
     pub(crate) fn hex_to_decimal(hex: &str) -> Result<i16, Error> {
         if hex.len() != 4 {
             eprintln!("[ERROR] Wrong length given ");
@@ -101,12 +136,18 @@ mod test_device {
             temperatures: "FFFE00010003FFFE00010003FFFE00010003FFFE00010003".to_string(),
             registered_at: Utc::now() - Duration::minutes(5),
         };
-        device.save_temperatures(cmd);
+        device.save_temperatures(cmd).unwrap();
 
         //THEN
         assert!(!device.temperatures.is_empty());
-        assert_eq!(device.temperatures.first().unwrap().temperature, -2);
-        assert_eq!(device.temperatures.last().unwrap().temperature, 3);
+        assert_eq!(
+            device
+                .temperatures
+                .into_iter()
+                .map(|t| t.temperature)
+                .collect::<Vec<_>>(),
+            vec![-2, 1, 3, -2, 1, 3, -2, 1, 3, -2, 1, 3]
+        );
     }
 
     #[test]
